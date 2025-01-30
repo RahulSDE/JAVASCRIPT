610119614907cbb182f379be58d56d5e6526d43a
RahulSDE <baaburahulsingh@gmail.com>
2025-01-31 00:01:52 +0530

    comments added

---
+++ c. Second_Pillar_Of_Javascript/_17_UseCase_Of_Named_FunctionExpression.js
@@ -0,0 +1,17 @@
+/*
+    Use Cases of Named Function Expression
+        -> it increases the readability.
+        -> for recursive cases named function expression is needed.
+        -> when we call console.trace() function then it gives us the call-stack of the function which are getting called.
+           Now if the function is not named, how will i get to know which function is in the call stack.
+*/
+
+function fun(fn){
+    console.log("Welcome to fun");
+    fn();
+}
+
+fun(function askingAboutFun(){
+    console.log("Too much fun!!!");
+    console.trace();
+})
--- c. Second_Pillar_Of_Javascript/_1_scope1.js
+++ c. Second_Pillar_Of_Javascript/_1_scope1.js
@@ -1,16 +1,17 @@
 console.log(teacher); 
 /* since teacher variable is already got the memory in parsing phase. Hence while execution it will show 
    undefined because it has not been initialized yet. It is initialized in execution phase.*/
-var teacher = "Rahul";
 
-function fun(){
-    var teacher = "Manish";
+var teacher = "Rahul";  // teacher -> global-scoped.
+
+function fun(){ // fun -> global-scoped.
+    var teacher = "Manish"; // teacher -> fun-scoped
     content = "Js";
     console.log(teacher);
 }
 
-function gun(){
-    var student = "Pawan";
+function gun(){ // gun -> global-scoped.
+    var student = "Pawan"; // student -> gun-scoped.
     console.log(student);
 }
 
